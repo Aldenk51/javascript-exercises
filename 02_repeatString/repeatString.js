e344ecfc1f2613b929d11707bb4c0c4b7e08df92
Alden Kim <aldenk51@gmail.com>
2023-02-23 22:32:54 -0500

    Finish jss exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(input, number) {
+    let output = "";
+    if (number < 0) return "ERROR";
+    for(;number > 0; number --) {
+        output += input;
+    }
+    return output;
 };
 
 // Do not edit below this line
