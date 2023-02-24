1a084f351bab695d73ff6c9ad2db0027ed33c9f0
Alden Kim <aldenk51@gmail.com>
2023-02-24 16:30:27 -0500

    finish js exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.replace(/[^A-Za-z]/g, "").toLowerCase();
+    for(let i = 0; i < input.length/2; i ++) {
+        if(input.charAt(i) != input.charAt(input.length-1 - i)) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
