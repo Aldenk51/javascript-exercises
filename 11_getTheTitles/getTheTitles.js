9c1afbab8df118c6ecb0a29e9b86cdb5869803ca
Alden Kim <aldenk51@gmail.com>
2023-02-24 17:08:56 -0500

    Finish js exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(input) {
+    const titles = [];
+    for(i of input) {
+        titles.push(i.title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
