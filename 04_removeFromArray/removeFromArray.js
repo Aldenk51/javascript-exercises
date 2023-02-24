eb41b6c861b1b45c72f57cc420f21cd4f9799154
Alden Kim <aldenk51@gmail.com>
2023-02-24 00:05:18 -0500

    Finish js exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...theArgs) {
+    console.log(input);
+    for (i of theArgs) {
+        if(input.indexOf(i) < 0) {console.log("not found");}
+        else {
+            if (typeof i === 'string') {
+                input.splice(input.indexOf(i), 1);
+                console.log("word");
+            } else {
+                input.splice(input.indexOf(i), 1);
+                console.log("number");
+            }
+        }
+        console.log(input);
+    }
+    return input;
 };
 
 // Do not edit below this line
