42871395a0e8845e8ee1177ce2db785fc7735d82
Alden Kim <aldenk51@gmail.com>
2023-02-24 00:19:43 -0500

    Fnish jss exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(input1, input2) {
 
+    if(Number.isInteger(input1) && Number.isInteger(input2) && input1 > 0 && input2 > 0) {
+        let sum = 0;
+        for(let i = Math.min(input1, input2); i <= Math.max(input1, input2); i ++) {
+            sum += i;
+        }
+        return sum;
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
