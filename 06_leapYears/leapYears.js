6edb0e72c86829b39f896b2af8eecb652aafbd69
Alden Kim <aldenk51@gmail.com>
2023-02-24 00:35:06 -0500

    Finish jss exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%4) {
+        return false;
+    }
+    if (!(year%400)) {
+        return true;
+    }
+    if(!(year%100)) {
+        console.log(1);
+        return false;
+    }
+    console.log(3);
+    return true;
 };
 
 // Do not edit below this line
