8cc4ea58c26d8252a94f4dff350823666d8f0dfe
Alden Kim <aldenk51@gmail.com>
2023-02-24 15:28:50 -0500

    Finish js exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(input1, input2) {
+	return input1 + input2;
 };
 
-const subtract = function() {
-	
+const subtract = function(input1, input2) {
+	return input1 - input2;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  let total = 0;
+	for(const i of input) {
+    total += i;
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  let total = 1;
+  for (const i of input) {
+    total *= i;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(input1, input2) {
+	return input1 ** input2;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  let total = 1;
+  for (; input > 0; input --) {
+    total *= input;
+  }
+  return total;
 };
 
 // Do not edit below this line
