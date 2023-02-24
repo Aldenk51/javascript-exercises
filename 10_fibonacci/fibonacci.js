255f50975d210fb0ed1ea8fc842583b08cafe8ab
Alden Kim <aldenk51@gmail.com>
2023-02-24 16:59:03 -0500

    Finish js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let a = 0, b = 1, c, i;
+    if (input < 0) return "OOPS";
+    if (input == 0) return a;
+    for(i = 2; i <= input; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
