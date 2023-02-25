08487bb7b5a697c2ed27eee1aac325b6373cf6ed
Alden Kim <aldenk51@gmail.com>
2023-02-24 20:55:13 -0500

    Finish js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    let oldestPos, oldestAge = 0, temp = 0;
+    for(i of input) {
+        if(!i.hasOwnProperty('yearOfDeath') && ((2023-i.yearOfBirth) > oldestAge)) {
+            oldestPos = temp;
+            oldestAge = 2023-i.yearOfBirth;
+        } else if ((i.yearOfDeath-i.yearOfBirth) > oldestAge) {
+            oldestPos = temp;
+            oldestAge = i.yearOfDeath-i.yearOfBirth;
+        }
+        temp ++;
+    }
+    return input[oldestPos];
 };
 
 // Do not edit below this line
